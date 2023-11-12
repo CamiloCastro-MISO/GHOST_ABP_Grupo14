cb5b821ff94c6421b112b2fc46443acd69bc99b0
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-12 04:14:22 -0500

    Finalizado: Funcionalidad Crear Miembros

--- CYPRESS/cypress/e2e/crear-miembro/class-crear-miembro.js
+++ CYPRESS/cypress/e2e/crear-miembro/class-crear-miembro.js
@@ -2,7 +2,7 @@ import config from "../../assets/config.json";
 import LoginPage from '../1-iniciar-sesion/1-class-inicio-sesion';
 import DashBoardItem from '../dashboard/class-dashboard';
 
-class PageItem {
+class MemberItem {
 
     checkMainPage() {
         let logIn = new LoginPage()
@@ -15,6 +15,53 @@ class PageItem {
         cy.wait(1000)
     }
 
+    goToMembers() {
+        cy.get('[data-test-nav="members"]').click();
+        cy.wait(1000);
+    }
+
+    newMember() {
+        cy.get('[data-test-new-member-button="true"]').click();
+        cy.wait(1000);
+    }
+
+    fillMember(name, mail) {
+        cy.get('#member-name').type(name);
+        cy.wait(1000);
+        cy.get('#member-email').type(mail);
+        cy.wait(1000);
+    }
+
+    saveMember() {
+        try {
+            cy.get('button[data-test-button="save"]').click();
+            cy.wait(1000);
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+    validate(text) {
+        cy.get('p.response').contains(text);
+        cy.wait(1000);
+    }
+
+    validateMember(name, email) {
+        cy.get('h3.gh-members-list-name').contains(name).should('be.visible');
+        cy.wait(1000);
+        cy.get('p.gh-members-list-email').contains(email).should('be.visible');
+        cy.wait(1000);
+    }
+
+    leave() {
+        cy.get('[data-test-leave-button]').click();
+        cy.wait(1000);
+    }
+
+    addYourself() {
+        cy.get('[data-test-button="add-yourself"]').click();
+        cy.wait(1000);
+    }
 }
 
-export default PageItem;
+export default MemberItem;
